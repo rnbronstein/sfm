64480a4d9c2d5ef8684b7ab156d4773e7d1e2020
rnbronstein <rnbronstein@gmail.com>
2016-07-02 00:17:08 -0400

    information rendering correctly

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   resources :users
+  resources :songs
+  resources :composers
+  resources :categories
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
